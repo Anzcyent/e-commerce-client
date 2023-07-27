a62c140ce33901221e06eb053646b6c0c73d8f32
Cihan <ozcancihan714@gmail.com>
2023-07-27 12:53:55 +0300

    Home Products Responsive

--- src/components/HomeProducts.jsx
+++ src/components/HomeProducts.jsx
@@ -5,10 +5,10 @@ const HomeProducts = () => {
   return (
     <section className="home-products md:w-5/6 w-full p-5 flex flex-wrap justify-evenly items-start overflow-y-auto gap-10">
       {products.map((product) => (
-        <div key={product.id} title={product.description} className="w-[200px] h-60 flex flex-col justify-evenly items-center border-2 border-darkBlue p-3 mr-5 hover-and-scale rounded-lg">
-          <img className="w-30 h-40 object-cover" src={product.image} alt={product.name} />
-          <span className="font-bold">{product.name}</span>
-          <span className="font-semibold text-lightBlue">${product.price}</span>
+        <div key={product.id} title={product.description} className="sm:w-[200px] w-[100px] sm:h-60 h-40 flex flex-col justify-evenly items-center border-2 border-darkBlue p-3 mr-5 hover-and-scale rounded-lg">
+          <img className="w-30 sm:h-40 h-20 object-cover" src={product.image} alt={product.name} />
+          <span className="font-bold sm:text-base text-xs">{product.name}</span>
+          <span className="font-semibold text-lightBlue sm:text-base text-sm">${product.price}</span>
         </div>
       ))}
     </section>
