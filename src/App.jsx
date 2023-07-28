307318be1cac52af9e52dc86d7e6ea2643867188
Cihan <ozcancihan714@gmail.com>
2023-07-28 16:51:41 +0300

    React Router Dom Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { Home, Login, Register, NotFound, Cart, Product } from './pages'
-import "./App.css"
+import React from "react";
+import { Home, Login, Register, NotFound, Cart, Product } from "./pages";
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
-  return (
-    <>
-      <Home />
-    </>
-  )
-}
+  const user = true;
 
-export default App
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/product/:id" element={<Product />} />
+      <Route exact path="/cart" element={<Cart />} />
+      <Route
+        exact
+        path="/login"
+        element={user ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        exact
+        path="/register"
+        element={user ? <Navigate to="/" /> : <Register />}
+      />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
+export default App;
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
-    <main className='font-urbanist w-[100vw] h-[100vh] flex flex-col justify-center items-center'>
-        <h1 className='text-4xl text-darkBlue font-bold tracking-wider'>404 Not Found</h1>
-        <button className='bg-lightBlue text-white px-3 py-1 mt-5 hover-and-scale'>Return Home</button>
+    <main className="font-urbanist w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <h1 className="text-4xl text-darkBlue font-bold tracking-wider">
+        404 Not Found
+      </h1>
+      <button
+        onClick={() => navigate("/")}
+        className="bg-lightBlue text-white px-3 py-1 mt-5 hover-and-scale"
+      >
+        Return Home
+      </button>
     </main>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
