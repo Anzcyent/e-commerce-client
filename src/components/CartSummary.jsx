9ddf4aedc3231e0d8792c7cf522a6f9d2c6d8169
Cihan <ozcancihan714@gmail.com>
2023-08-03 11:29:15 +0300

    Stripe Checkout

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import StripeCheckOut from "react-stripe-checkout";
 
-const CartSummary = ({total}) => {
+const CartSummary = ({ total }) => {
+  const [stripeToken, setStripeToken] = useState(null);
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
   const discount = 3;
   return (
     <section className="flex items-center justify-center h-full md:mt-0 mt-5">
@@ -9,11 +16,30 @@ const CartSummary = ({total}) => {
           Taxes: <span className="font-bold text-lightBlue">4%</span>
         </span>
         <span className="mb-5">
-          Discount: <span className="font-bold text-lightBlue">{discount}%</span>
+          Discount:{" "}
+          <span className="font-bold text-lightBlue">{discount}%</span>
         </span>
-        <span>
-          Total: <span className="text-darkBlue font-bold">${Math.floor(total) - (Math.floor(total) * discount / 100)}</span>
+        <span className="mb-5">
+          Total:{" "}
+          <span className="text-darkBlue font-bold">
+            ${Math.floor(total) - (Math.floor(total) * discount) / 100}
+          </span>
         </span>
+
+        <StripeCheckOut
+          name="ShopEazy"
+          image="https://media.istockphoto.com/id/157308559/tr/foto%C4%9Fraf/money-pile-100-dollar-bills.jpg?s=612x612&w=0&k=20&c=48StwAO8TerBkLxDUBn5WCSr4BHYb1ov1Sg67VIcup0="
+          billingAddress
+          shippingAddress
+          description={`Your total is $${(Math.floor(total) - (Math.floor(total) * discount) / 100)}`}
+          amount={(Math.floor(total) - (Math.floor(total) * discount) / 100) * 100}
+          token={onToken}
+          stripeKey="pk_test_51NUTNoHBvpeGrdzgigkOVnUi6rg8IzthW3dwQgEYsWClcRFi9W2LEUMGRiawA7G8YqU3eNxeoA6uqAKjg3D57Mmk00nfsfHDWO"
+        >
+          <button className="bg-darkBlue text-white px-3 py-1 hover-and-scale">
+            CHECKOUT NOW
+          </button>
+        </StripeCheckOut>
       </div>
     </section>
   );
